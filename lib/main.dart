9e0325fae0276803a8aa873bffd0572651b6b5dc
Halo Architect <halo-architect@example.com>
2026-04-10 10:42:02 +0000

    fix: remove unused import to fix analyze

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
